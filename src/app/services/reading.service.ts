5c103096d5df8b8067efa963d52d8e0a047eadbc
Firebird07 <krause03.jk@gmail.com>
2025-01-28 08:52:31 +0100

    Reading Edit hinzugefügt

--- src/app/services/reading.service.ts
+++ src/app/services/reading.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Reading} from '../types/reading';
+import { KindOfMeter } from '../enums/kind-of-meter';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,21 @@ export class ReadingService {
 
     public store(customer: string, dateOfReading: string, meterId: string, meterCount: string, kindOfMeter: string, comment: string, substitute?: string,): Observable<Reading> {
       return this.http.post<Reading>(this.url, {customer, dateOfReading, meterId, meterCount, kindOfMeter, comment, substitute});
+    }
+
+    public update(id: string, customerId: string, dateOfReading: Date, meterId: string, meterCount: number, kindOfMeter: KindOfMeter, comment: string, substitute?: boolean): Observable<string> {
+      const formattedDateOfReading: string | undefined = dateOfReading?.toISOString().substring(0, 10);
+  
+      return this.http.put<string>(this.url,
+        {
+          id,
+          customerId,
+          dateOfReading: formattedDateOfReading,
+          meterId,
+          meterCount,
+          kindOfMeter,
+          comment,
+          substitute
+        });
     }
 }
