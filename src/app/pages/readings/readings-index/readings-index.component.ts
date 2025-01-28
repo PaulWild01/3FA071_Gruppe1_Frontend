f7d2cda35060067a7625b8adb1bf440e77de6e8e
Julian <krause03.jk@gmail.com>
2025-01-28 14:38:37 +0100

    Refactor

--- src/app/pages/readings/readings-index/readings-index.component.ts
+++ src/app/pages/readings/readings-index/readings-index.component.ts
@@ -1,21 +1,24 @@
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {NgForOf} from '@angular/common';
-import {ReadingService} from '../../../services/reading.service';
-import {Reading} from '../../../types/reading';
+import {ReadingService} from '../services/reading.service';
+import {Reading} from '../types/reading';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
-  selector: 'app-customer-show',
+    selector: 'app-customer-show',
   imports: [
     NgForOf,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
-  templateUrl: './readings-index.component.html',
+    templateUrl: './readings-index.component.html',
+    styleUrl: './readings-index.component.css'
 })
 export class ReadingsIndexComponent {
-  public readings: Reading[] = [];
+    public readings: Reading[] = [];
 
-  constructor(private readingService: ReadingService) {
-    readingService.all().subscribe(readings => this.readings = readings)
-  }
-}
+      constructor(private readingsservice: ReadingService) {
+          readingsservice.all().subscribe(readings => this.readings = readings)
+        }
+    }
