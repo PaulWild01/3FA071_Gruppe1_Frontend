15455a67e300130d4407059fd8fd31d45126dc08
Firebird07 <106547542+Firebird07@users.noreply.github.com>
2025-01-31 12:36:02 +0100

    XML Import Customer angelegt

--- src/app/components/confirmation-modal/import-modal.ts
+++ src/app/components/confirmation-modal/import-modal.ts
@@ -1,6 +1,7 @@
 import {Component, inject, Input} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {Customer} from '../../types/customer';
+import {Gender} from '../../enums/gender';
 
 @Component({
   selector: 'app-confirmation-modal',
@@ -44,15 +45,53 @@ export class ImportModalComponent {
       const reader = new FileReader();
       reader.onload = () => {
         const content = reader.result as string;
-        try {
-          this.filedata = JSON.parse(content);
-          console.log("Success" + this.filedata);
+        if (file.name.endsWith(".json")) {
+          this.parseJSON(content);
         }
-        catch (err) {
-          console.log(err);
+        else if (file.name.endsWith(".xml")) {
+          this.parseXML(content);
         }
       };
       reader.readAsText(file);
     }
+  }
+
+  parseJSON(content: string): void {
+    try {
+      this.filedata = JSON.parse(content);
+    } catch (err) {
+      console.error('Fehler beim Parsen der JSON-Datei:', err);
+      alert('Invalid JSON file');
+    }
+  }
+
+  parseXML(content: string): void {
+    try {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(content, "text/xml");
+      const customers = xmlDoc.getElementsByTagName("customer");
+      this.filedata = [];
+
+      for (const customer of Array.from(customers)) {
+        const id = customer.getElementsByTagName('id')[0]?.textContent || '';
+        const firstName = customer.getElementsByTagName('firstName')[0]?.textContent || '';
+        const lastName = customer.getElementsByTagName('lastName')[0]?.textContent || '';
+        const gender = this.toGender(customer.getElementsByTagName('gender')[0]?.textContent || '');
+        const birthDate = customer.getElementsByTagName('birthDate')[0]?.textContent || undefined;
+
+        this.filedata.push({id, firstName, lastName, gender, birthDate})
+      }
+    }
+    catch (error) {
+      console.error('Fehler beim Parsen der XML-Datei:', error);
+      alert('Invalid XML file');
+    }
+  }
+
+  private toGender(value: string): Gender {
+    if (Object.values(Gender).includes(value as Gender)) {
+      return value as Gender;
+    }
+    throw new Error(`Ungültiger Gender-Wert: ${value}`);
   }
 }
