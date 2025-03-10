91bb853eb5aab377c46f7e45fdec544561b1ac60
Paul Wild <paul.wild01@gmx.de>
2025-03-10 11:23:00 +0100

    add dark mode toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NgIcon} from '@ng-icons/core';
+import {NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    NgIcon,
+    NgbTooltip,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
+  protected readonly document = document;
+
+  theme = signal<'light' | 'dark'>('light');
+  tooltip = computed(() => `Switch to ${this.theme() === 'light' ? 'dark' : 'light'} mode`)
+
+  toggleDarkmode() {
+    const html = document.querySelector('html')!;
+    this.theme.update(oldTheme => oldTheme === 'dark' ? 'light': 'dark');
+
+    html.setAttribute('data-bs-theme', this.theme());
+    window.localStorage.setItem('theme', this.theme());
+  }
+
+  constructor() {
+    this.theme.set(window.localStorage.getItem('theme') as 'dark' | 'light' ?? 'light');
+    document.querySelector('html')!.setAttribute('data-bs-theme', this.theme());
+  }
 }
