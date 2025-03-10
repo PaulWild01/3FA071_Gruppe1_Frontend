9c3685c3a46da1aff14b1bce874a49738dcf027c
Knowiii <annabel.amon@gmail.com>
2025-03-10 12:23:18 +0100

    added altered color-theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,9 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
     RouterLinkActive
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
 }
+
