77c511675e6bff685ae8fc3d43956ed4436decf7
Paul Wild <paul.wild01@gmx.de>
2025-03-21 12:54:51 +0100

    remove unused and commented out code

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,7 +1,5 @@
 import {Injectable} from '@angular/core';
 import {Customer} from '../types/customer';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
 import {ApiRessourceService} from './api-ressource.service';
 import {CustomerData} from '../types/customer.data';
 
@@ -10,45 +8,4 @@ import {CustomerData} from '../types/customer.data';
 })
 export class CustomerService extends ApiRessourceService<Customer, CustomerData> {
   override path = 'customers'
-  // private url = "http://localhost:8080/customers";
-  //
-  // constructor(private http: HttpClient) {
-  // }
-  //
-  // public all(): Observable<Customer[]> {
-  //   return this.http.get<Customer[]>(this.url);
-  // }
-  //
-  // public findById(id: string): Observable<Customer> {
-  //   return this.http.get<Customer>(`${this.url}/${id}`);
-  // }
-  //
-  // public store(firstName: string, lastName: string, gender: string, birthDate?: Date): Observable<Customer> {
-  //   const formattedBirthdate: string | undefined = birthDate?.toISOString().substring(0, 10);
-  //
-  //   return this.http.post<Customer>(this.url,
-  //     {
-  //       firstName,
-  //       lastName,
-  //       gender,
-  //       birthDate: formattedBirthdate,
-  //     });
-  // }
-  //
-  // public update(id: string, firstName: string, lastName: string, gender: string, birthDate?: Date): Observable<string> {
-  //   const formattedBirthdate: string | undefined = birthDate?.toISOString().substring(0, 10);
-  //
-  //   return this.http.put<string>(this.url,
-  //     {
-  //       id,
-  //       firstName,
-  //       lastName,
-  //       gender,
-  //       birthDate: formattedBirthdate,
-  //     });
-  // }
-  //
-  // public destroy(id: string): Observable<string> {
-  //   return this.http.delete<string>(`${this.url}/${id}`);
-  // }
 }
