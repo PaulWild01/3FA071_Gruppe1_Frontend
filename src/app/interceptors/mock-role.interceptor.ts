eb66f33eddb46a5772cdf0385c090e06feb2fd03
Paul Wild <paul.wild01@gmx.de>
2025-03-21 12:46:07 +0100

    create customer from new reading flow

--- src/app/interceptors/mock-role.interceptor.ts
+++ src/app/interceptors/mock-role.interceptor.ts
@@ -1,8 +1,7 @@
 import {HttpInterceptorFn, HttpResponse} from '@angular/common/http';
-import {map, of} from 'rxjs';
+import {of} from 'rxjs';
 import {User} from '../types/user';
 import {Gender} from '../enums/gender';
-import {Customer} from '../types/customer';
 
 const admin: User = {id: '49ebadf8-b64e-4b7d-8d98-000180558d7e', username: 'Admin', role: 'Admin'};
 const customer: User = {id: '49ebadf8-b64e-4b7d-8d98-000180558d7f', username: 'Customer', role: 'Customer', customer: {id: '49ebadf8-b64e-4b7d-8d98-000180558d7d', gender: Gender.M, firstName: 'Siegmund', lastName: 'Schneider', birthDate: undefined}};
